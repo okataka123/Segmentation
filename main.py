06731c69fd811e32d9f96d35ece025589bd7bb4e
okataka123 <tekepika@gmail.com>
2024-09-24 00:56:02 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import argparse 
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from models.UNet import *
@@ -53,16 +54,6 @@ test_loader = DataLoader(
 #    num_workers=4
 )
 
-# train
-model = UNet(n_channels=3, n_classes=21) # Pascal VOCの場合
-#criterion = torch.nn.BCELoss()
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
-
-
-# start training
-best_loss = 10**5
-epochs = 20
 
 def vis_dataset():
     # datasetの中身を確認
@@ -97,40 +88,66 @@ def visualization_loss(train_loss_value):
     plt.legend()
     plt.show()
 
+def train(savemodel=False):
+    model = UNet(n_channels=3, n_classes=21) # Pascal VOCの場合
+    #criterion = torch.nn.BCELoss()
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    epochs = 20
 
-train_loss_value = []
+    train_loss_value = []
+    for epoch in tqdm(range(epochs)):
+        print(f'==========Epoch {epoch+1} Start Training==========')
+        model.train()
+        train_loss = 0
+        for images, targets in tqdm(train_loader, total=len(train_loader)):
+            optimizer.zero_grad()
+            outputs = model(images)
+            
 
-for epoch in tqdm(range(epochs)):
-    print(f'==========Epoch {epoch+1} Start Training==========')
-    model.train()
-    train_loss = 0
-    for images, targets in tqdm(train_loader, total=len(train_loader)):
-        optimizer.zero_grad()
-        outputs = model(images)
-        
+            # print('outputs[0].shape =', outputs[0].shape)
+            # print('targets[0].shape =', targets[0].shape)
 
-        # print('outputs[0].shape =', outputs[0].shape)
-        # print('targets[0].shape =', targets[0].shape)
+            # print('targets[0] =', targets[0])
 
-        # print('targets[0] =', targets[0])
+            # print('outputs.shape =', outputs.shape)
+            # print('targets.shape =', targets.shape)
 
-        # print('outputs.shape =', outputs.shape)
-        # print('targets.shape =', targets.shape)
+            targets_2 = targets.squeeze(1)
+            targets_2 = targets_2.long()
+            # print('targets_2.shape =', targets_2.shape)
+            # print('set(targets_2) =', set(targets_2))
 
-        targets_2 = targets.squeeze(1)
-        targets_2 = targets_2.long()
-        # print('targets_2.shape =', targets_2.shape)
-        # print('set(targets_2) =', set(targets_2))
-
-        #loss = criterion(outputs, targets)
-        loss = criterion(outputs, targets_2)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-    train_loss_value.append(train_loss/len(train_loader))
+            #loss = criterion(outputs, targets)
+            loss = criterion(outputs, targets_2)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+        train_loss_value.append(train_loss/len(train_loader))
     visualization_loss(train_loss_value)
+    if savemodel:
+        pass
 
 
+# 推論
+def inference():
+    pass
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Train or run inference on the model.')
+    parser.add_argument('--train', action='store_true', help="Run training mode.")
+    parser.add_argument('--inference', action='store_true', help="Run inference mode.")
+    
+    args = parser.parse_args()
+    if args.train and args.inference:
+        print("Error: Please specify either --train or --inference, not both.")
+    elif args.train:
+        train(savemodel=True)
+    elif args.inference:
+        inference()
+    else:
+        print("Error: Please specify either --train or --inference.")
 
+if __name__ == '__main__':
+    main()
